0f9d02f3f42c8eb7481dd6c084ce027a57aa43fe
Alexander Tuna <alexander.tuna@gmail.com>
2024-09-30 13:06:05 -0700

    Uncomment publisher

--- src/main.py
+++ src/main.py
@@ -143,4 +143,4 @@ if trig.predictSmoke(model_type, execute):
 
 logging.info('Publish')
 publisher_waggle = publisher.PublisherWaggle(model_type,execute)
-# publisher_waggle.publish(sage_data_topic,smoke_threshold,camera_src)
+publisher_waggle.publish(sage_data_topic,smoke_threshold,camera_src)
